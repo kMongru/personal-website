9c6f7017f3be5de17ab18197976a46c4ae332f18
Keegan Mongru <kmongru78@gmail.com>
2021-12-31 21:18:12 -0500

    add folders and hero images

---
+++ src/Pages/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../Components/Header/Header';
+
+const Landing = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Landing;
