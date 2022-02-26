0d309e19de3950d1fe719ede610b8b269421fffb
Keegan Mongru <kmongru78@gmail.com>
2022-02-25 21:24:31 -0500

    add style to timeline

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -3,46 +3,30 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from 'react-vertical-timeline-component';
+import { ReactComponent as Icon } from '../../assets/Hobby.svg';
 
 //styling
+import 'react-vertical-timeline-component/style.min.css';
 import './timeline.css';
 
 const Timeline = ({ data }) => {
   return (
     <VerticalTimeline>
       {data.map((item) => {
-        let Icon = require(`../../assets/${item.iconName}.svg`);
         return (
           <VerticalTimelineElement
-            key={item.title}
-            className='vertical-timeline-element--work'
-            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-            date='2011 - present'
+            key={item.id}
+            date={item.date}
+            dateClassName='timeline-date'
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={Icon}
+            icon={<Icon />}
           >
             <h3 className='vertical-timeline-element-title'>{item.title}</h3>
-            <h4 className='vertical-timeline-element-subtitle'>Miami, FL</h4>
-            <p>{item.bodyText}</p>
+            <h4 className='vertical-timeline-element-subtitle'>
+              {item.organization}
+            </h4>
+            <p id='description'>{item.description}</p>
           </VerticalTimelineElement>
-          // <VerticalTimelineElement
-          //   key={item.frontmatter.title}
-          //   date={item.frontmatter.date}
-          //   iconStyle={{
-          //     background: item.frontmatter.background,
-          //     color: item.frontmatter.foreground,
-          //   }}
-          //   icon={<Icon />}
-          // >
-          //   {/* <Typography variant='h5' className='vertical-timeline-element-title'>
-          //     {item.frontmatter.title}
-          //   </Typography>
-          //   <Typography variant='h6' className='vertical-timeline-element-subtitle'>
-          //     {item.frontmatter.subtitle}
-          //   </Typography> */}
-          //   <div dangerouslySetInnerHTML={{ __html: item.html }} />
-          // </VerticalTimelineElement>
         );
       })}
     </VerticalTimeline>
--- src/pages/about/AboutPage.js
+++ src/pages/about/AboutPage.js
@@ -1,16 +1,47 @@
 import React from 'react';
 
-import wip_animation from '../../assets/wip_animation.gif';
 import Wrapper from '../Wrapper';
-import { Timeline } from '../../components';
+import { Timeline, TextCard } from '../../components';
 import data from '../../data/timeline.json';
+import headshot from '../../assets/headshot.JPG';
 
+//styling
 import './aboutPage.css';
 
-const AboutPage = (props) => {
+const AboutPage = () => {
   return (
     <Wrapper>
-      <div className='about-flex'>
+      <div className='about-row'>
+        <div className='about-column'>
+          <h3>Welcome!</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar
+            proin gravida hendrerit lectus a. Nunc sed augue lacus viverra vitae
+            congue eu. Elit at imperdiet dui accumsan sit amet nulla facilisi
+            morbi.
+          </p>
+          <div className='about-row'>
+            <TextCard
+              title='Languages'
+              content='Java
+            Javascript
+            Python
+            C#'
+            />
+            <TextCard
+              title='Tools & Frameworks'
+              content='React
+            React Native
+            Figma
+            GitHub'
+            />
+          </div>
+        </div>
+        <img src={headshot} alt='headshot' />
+      </div>
+      <h3>Timeline</h3>
+      <div className='timeline_container'>
         <Timeline data={data} />
       </div>
     </Wrapper>
